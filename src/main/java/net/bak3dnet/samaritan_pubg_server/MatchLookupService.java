691c9f3240a503665bfbd51131f72f7f78ad990f
Lainkai <Lainkai@users.noreply.github.com>
2019-03-05 14:53:32 -0500

    Lookup service stuff

--- src/main/java/net/bak3dnet/samaritan_pubg_server/MatchLookupService.java
+++ src/main/java/net/bak3dnet/samaritan_pubg_server/MatchLookupService.java
@@ -1,5 +1,8 @@
 package net.bak3dnet.samaritan_pubg_server;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -11,15 +14,38 @@ import org.springframework.stereotype.Service;
 public class MatchLookupService {
 
     private ArrayList<String> playersToGet = new ArrayList<String>();
+    public static String apiKey;
 
     @Async
     public CompletableFuture<Map<String, Player>> get6PlayerMatches() throws InterruptedException {
         while(true) {
             if(!playersToGet.isEmpty()) {
                 
-                for() {}
+                String preURL = "https://api.pubg.com/shards/steam/players?filter[playerNames]=";
 
-                Thread.sleep(6000);
+                for(int i = 0; i < 6; i++) {
+
+                    preURL += playersToGet.remove(0);
+
+                    if(playersToGet.size() == 0) {
+
+                        break;
+
+                    }
+
+                    preURL += ",";
+
+                }
+
+                URL url = new URL(preURL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Authorization",String.format("Bearer %s",apiKey));
+                conn.setRequestProperty("Accept", "application/vnd.api+json");
+
+                InputStream jIS = conn.getInputStream();
+
+                Thread.sleep(/*TODO Time*/);
                 return CompletableFuture.completedFuture();
             }
         }
